a0dd1fd496594dbfba4577684389eaad100eca95
jeremyeliud <jeremyeliud524@gmail.com>
2023-03-15 15:21:33 +0300

    The First Commit

---
+++ datatype.py
@@ -0,0 +1,14 @@
+num=10 #int
+num2=5.467 #float
+firstname="Eliud" #string
+IsGood=True #bool
+
+print(type(num))
+print(type(num2))
+print(type(firstname))
+print(type(IsGood))
+
+print(float(num))
+print(int(num2))
+
+
---
+++ strings.py
@@ -0,0 +1,12 @@
+firstname="Technology"
+lastname="Training"
+print(firstname+ ","+lastname)
+age= "twenty"
+agevalue= "years"
+
+valx=19
+valy=20
+print("My name is "+firstname)
+print("My age is "+ age +" "+agevalue)
+
+print(valx, valy)
